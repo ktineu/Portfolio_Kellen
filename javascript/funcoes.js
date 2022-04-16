fce46945dccca83b1733edf018f5df02792c5bc6
Kellen <ktineuc@gmail.com>
2022-04-16 19:28:24 -0300

    Salvo arquivos do portfolio Kellen

---
+++ javascript/funcoes.js
@@ -0,0 +1,35 @@
+
+function apareceFoto(tipo) {
+
+    if (tipo==1){
+
+        document.getElementById("icone").src="img/casa.png";
+    }
+    if (tipo==2){
+
+        document.getElementById("icone").src="img/emoj.png";
+    }
+    if (tipo==3){
+
+        document.getElementById("icone").src="img/icone_servicos.png";
+    }
+    if (tipo==4){
+        document.getElementById("icone").src="img/icone_galeria.png";
+    }
+    if (tipo==5){
+
+        document.getElementById("icone").src="img/icone_contato.png";
+    }
+    if (tipo==6){
+
+        document.getElementById("icone").src="img/icone_comentario.png";
+    }
+    if (tipo==7){
+
+        document.getElementById("icone").src="";
+    }
+
+    
+}
+
+    
